e75a4d1dcf7a09eb22752422dead45054acff3df
Ahmed M <devahmedm7@gmail.com>
2023-10-21 12:35:21 +0500

    finished working on the logic for checkout page

--- src/assets/components/Checkout/CheckOutItem.component.jsx
+++ src/assets/components/Checkout/CheckOutItem.component.jsx
@@ -30,34 +30,42 @@ const CheckOutItem = ({ item, index }) => {
   };
 
   return (
-    <div>
-      <div className="flex flex-row h-40 p-2 justify-between " key={item.id}>
-        <img src={item.image} alt={item.name} className="w-1/4" />
-        <div className="flex flex-col p-2">
-          <p>{item.name}</p>
-          <p>{item.price}</p>
+    <>
+      <div>
+        {/*  first div*/}
+        <div className="flex flex-row h-40 p-2 justify-between " key={item.id}>
+          <img src={item.image} alt={item.name} className="w-1/4" />
+          <div className="flex flex-col p-2">
+            <p>{item.name}</p>
+            <p>${item.price}</p>
+          </div>
+          {/*  Second div*/}
+
+          <div className="flex flex-row px-6 py-2 text-xl h-max border">
+            <p
+              onClick={handleDecrementClick}
+              className="text-2xl pr-4 hover:cursor-pointer"
+            >
+              {"-"}
+            </p>
+            <p className="px-4 ">{item.quantity}</p>
+            <p
+              onClick={handleIncrementClick}
+              className="text-2xl pl-4 hover:cursor-pointer"
+            >
+              {"+"}
+            </p>
+          </div>
+          {/*  Third div*/}
+
+          <div>
+            <p>${(item.quantity * item.price).toFixed(2)} </p>
+          </div>
+          <button onClick={handleOnDeleteButton}>Delete</button>
         </div>
-        <div className="flex flex-row p-4 text-xl">
-          <p
-            onClick={handleDecrementClick}
-            className="p-2 hover:cursor-pointer"
-          >
-            {"<"}
-          </p>
-          <p className="p-2 hover:curos">{item.quantity}</p>
-          <p
-            onClick={handleIncrementClick}
-            className="p-2 hover:cursor-pointer"
-          >
-            {">"}
-          </p>
-        </div>
-        <div>
-          <p>${(item.quantity * item.price).toFixed(2)} </p>
-        </div>
-        <button onClick={handleOnDeleteButton}>Delete</button>
       </div>
-    </div>
+      <div></div>
+    </>
   );
 };
 
--- src/assets/components/Checkout/Checkout.component.jsx
+++ src/assets/components/Checkout/Checkout.component.jsx
@@ -3,14 +3,19 @@ import { cartContext } from "../../../Contexts/Cart.context";
 import { userContext } from "../../../Contexts/user.context";
 import { Link } from "react-router-dom";
 import CheckOutItem from "./CheckOutItem.component";
+import Button from "../Minor Components/Button.component";
 
 const CheckOut = () => {
   const { cartItems } = useContext(cartContext);
   const { currentUser } = useContext(userContext);
 
-  //   if (!currentUser) {
-  //     return <h1>Please log in</h1>;
-  //   }
+  //Function to calculate total cost on Checkout page
+  const totalCost = cartItems
+    .reduce((acc, item) => {
+      return acc + item.quantity * item.price;
+    }, 0)
+    .toFixed(2);
+
   return (
     <div className="mx-60 p-4">
       <div className="flex flex-row justify-between p-16 ">
@@ -19,9 +24,21 @@ const CheckOut = () => {
           Continue Shopping
         </Link>
       </div>
+      {cartItems.length > 0 ? null : <div>Your Cart is Empty</div>}
       {cartItems.map((item, index) => {
         return <CheckOutItem key={item.id} item={item} index={index} />;
       })}
+      {totalCost > 0 ? (
+        <div className="p-4 flex flex-col">
+          <p className="ml-auto py-2">Subtotal: ${totalCost}</p>
+          <p className="ml-auto py-2">
+            Taxes and shipping calculated at checkout
+          </p>
+          <span className="ml-auto">
+            <Button>Checkout</Button>
+          </span>
+        </div>
+      ) : null}
     </div>
   );
 };
